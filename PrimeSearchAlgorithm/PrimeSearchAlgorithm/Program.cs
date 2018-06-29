87346ad7d71c67106da4ef899b7e36ae2fa4f12a
KyleBerdeguez <40679917+KyleBerdeguez@users.noreply.github.com>
2018-06-28 22:15:27 -0400

    All main program files.

---
+++ PrimeSearchAlgorithm/PrimeSearchAlgorithm/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            primes.Add(3);
+            int currentNumber = 4;
+            while (currentNumber < 100000)
+            {
+                bool isPrime = true;
+                int innerWhileIndex = 0;
+                while (innerWhileIndex < primes.Count - 1)
+                { 
+                    if (currentNumber % primes[innerWhileIndex] == 0)
+                    {
+                        isPrime = false;
+                    }
+                    innerWhileIndex++;
+                }
+                if (isPrime)
+                {
+                    primes.Add(currentNumber);
+                }
+                currentNumber++;
+            }
+            int lastWhile = 0;
+            while (lastWhile < primes.Count)
+            {
+                Console.WriteLine(primes[lastWhile]);
+                lastWhile++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
